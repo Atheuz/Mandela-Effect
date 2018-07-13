329009d3b82e04f1ce9c3a10509e13608a8e0891
Lasse Gravesen <lasse.gravesen@falcon.io>
2018-07-13 22:03:04 +0200

    Make a more comprehensive list.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Quick little Mandela Effect flip-flop hashing."""
+from datetime import date
 from datetime import datetime
 from hashlib import blake2b
 
@@ -10,7 +11,27 @@ FLIPFLOPS = [("Houston, we've had a problem", 'Houston, we have a problem'),
              ('Rice Krispies', 'Rice Crispy'),
              ('Bud light', 'Bud lite'),
              ('Reba McEntire', 'Reba McIntyre'),
-             ('Chick-fil-A', 'Chic-fil-A'), ]
+             ('Chick-fil-A', 'Chic-fil-A'),
+             ('Sex and the City', 'Sex in the City'),
+             ('Life was like a box of chocolates',
+              'Life is like a box of chocolates'),
+             ('Monopoly man does not have a monocle', 'Monopoly man has a monocle'),
+             ('JCPenney', 'JCPenny'),
+             ('NOT of the world', 'of the world'),
+             ('Mona Lina smirks', 'Mona Lisa does not smirk'),
+             ('KitKat', 'Kit-Kat'),
+             ('Interview with the Vampire', 'Interview with a Vampire'),
+             ('Magic Mirror on the Wall', 'Mirror, Mirror on the Wall'),
+             ('Pikachu tail = all yellow', 'Pikachu tail = yellow with black tip'),
+             ('Jif Peanut Butter', 'Jiffy Peanut Butter'),
+             ('Im a barbie girl, in the barbie world',
+              'Im a barbie girl, in a barbie world'),
+             ('Daylight Saving Time', 'Daylight Savings Time'),
+             ('Looney Tunes', 'Looney Toons'),
+             ('Pixie Stix', 'Pixy Stix'),
+             ('Chartreuse = Green', 'Chartreuse = Reddish'),
+             ('Febreze', 'Febreeze'),
+             ('Cup O Noodles', 'Cup Noodles'), ]
 
 
 def main():
@@ -22,7 +43,7 @@ def main():
     hexdigest = h.hexdigest()
     now = datetime.now()
     msg = "dt: {0} - hash: {1}\n".format(now, hexdigest)
-    with open("HASH", "w") as f:
+    with open("HASH-{}".format(date.today().isoformat()), "w") as f:
         print(msg)
         f.write(msg)
 
